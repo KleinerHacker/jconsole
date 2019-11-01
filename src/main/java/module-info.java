df6034829c2fe33ad4db9cf05c85da39c7396203
KleinerHacker <Kleiner_Hacker@freenet.de>
2019-11-01 21:33:59 +0100

    update

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.pcsoft.framework.jconsole {
+    requires commons.lang;
+    requires jna;
+
+    exports org.pcsoft.framework.jconsole;
+    exports org.pcsoft.framework.jconsole.type;
+    exports org.pcsoft.framework.jconsole.utils;
+}
